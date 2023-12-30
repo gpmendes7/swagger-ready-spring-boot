6c4de612469889ff24df69f5a8933a0e4f2c52a1
gpmendes7 <andrewsseven@gmail.com>
2023-12-30 10:02:56 -0300

    first commit

---
+++ src/main/java/com/example/swaggerready/http/response/Greeting.java
@@ -0,0 +1,13 @@
+package com.example.swaggerready.http.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+
+  private String message;
+}
---
+++ src/test/java/com/example/swaggerready/SwaggerReadyApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.swaggerready;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwaggerReadyApplicationTests {
+
+  @Test
+  public void contextLoads() {
+    SwaggerReadyApplication.main(new String[]{});
+  }
+
+}
